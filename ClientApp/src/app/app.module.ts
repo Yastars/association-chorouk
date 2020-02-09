772b16e8c79d7d8fce74c3f53f9906cc6e43efa1
yassine-qassar <yassine.qassar@fmcglab.com>
2020-02-09 05:08:22 +0100

    Angular Components (Setup)

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { GameComponent } from './game/game.component';
+import { GameRegistrationComponent } from './game-registration/game-registration.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    GameListComponent,
+    GameComponent,
+    GameRegistrationComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
