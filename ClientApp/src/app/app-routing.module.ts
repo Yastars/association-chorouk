71ad6ee04c5249457e5d2a28332f83237bad477a
yassine-qassar <yassine.qassar@fmcglab.com>
2020-03-04 05:31:47 +0100

    About Component

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
 
 
 const routes: Routes = [
 
   { path: '', component: DashboardComponent },
-  { path: 'about', component: DashboardComponent },
+  { path: 'about', component: AboutPageComponent },
   { path: 'blog', component: DashboardComponent },
 ];
 
