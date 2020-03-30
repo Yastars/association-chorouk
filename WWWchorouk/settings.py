875133fc44b1ac298d888c453a18cedd692ddcca
Yassine QS <yasstars@hotmail.fr>
2020-03-30 13:05:12 +0200

    changes

--- WWWchorouk/settings.py
+++ WWWchorouk/settings.py
@@ -143,10 +143,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
@@ -174,12 +186,6 @@ REST_FRAMEWORK = {
 }
 
 # Start Test Deploy
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # End Test Deploy
