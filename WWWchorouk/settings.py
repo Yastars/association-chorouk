93f502cf19c580dfd5b286b93d562b4abc93391c
Yassine QS <yasstars@hotmail.fr>
2020-04-01 10:34:32 +0200

    Test

--- WWWchorouk/settings.py
+++ WWWchorouk/settings.py
@@ -150,8 +150,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
